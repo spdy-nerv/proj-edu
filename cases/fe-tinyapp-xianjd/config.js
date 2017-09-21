3cbc0a363b9b7a0d6911962b3ec49e489bf1522b
梁冬 <liangdong@liangdongdeMacBook-Pro.local>
2017-09-21 21:51:28 +0800

    add case

---
+++ cases/fe-tinyapp-xianjd/config.js
@@ -0,0 +1,7 @@
+// config
+module.exports = {
+	caseName: 'fe-tinyapp-xianjd',
+	themeName: 'xianjd',
+	domainPrefix: 'https://microcloudtech.com/gateway/xajiaoda',
+	syncGlob: ['base/**/*', '!base/config.js', '!base/resources/*']
+};
