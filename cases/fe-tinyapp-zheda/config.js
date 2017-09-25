3b14397f46168e5d4b4e68e135a1377b039631f9
jp93 <1157852061@qq.com>
2017-09-25 13:00:04 +0800

    add case

---
+++ cases/fe-tinyapp-zheda/config.js
@@ -0,0 +1,7 @@
+// config
+module.exports = {
+	caseName: 'fe-tinyapp-zheda',
+	themeName: 'zheda',
+	domainPrefix: 'https://microcloudtech.com/gateway/zheda',
+	syncGlob: ['base/**/*', '!base/config.js', '!base/resources/*']
+};
